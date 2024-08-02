aceeb9acd9953d81845a28959a69ca14237c6c6f
Shan AZIZ <shan.azizh@gmail.com>
2024-08-02 14:37:02 +0200

    feat: all open jpa

--- src/main/kotlin/fr/device/api/device/Device.kt
+++ src/main/kotlin/fr/device/api/device/Device.kt
@@ -3,15 +3,17 @@ package fr.device.api.device
 import jakarta.persistence.*
 import java.time.Instant
 
-// TODO: améliorer la compat.
 @Entity
 data class Device(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long? = null,
     var name: String? = null,
+
     @Column(nullable = false)
     var buyingDate: Instant? = null,
+
     @Column(nullable = false)
     var buyingPrice: Double? = null,
+
     @Column(nullable = false)
     var annualEnergyConsumption: Double? = null,
 )
